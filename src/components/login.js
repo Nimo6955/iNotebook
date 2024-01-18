baced09c70d277c2cdbcc2e1c0845d990810027e
Sukhpal <singhsukhpal20012001@gmail.com>
2024-01-18 17:27:29 +0530

    complete

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,7 @@ function Login(props) {
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+    const response = await fetch(`${process.env.REACT_APP_BackEnd}/api/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
